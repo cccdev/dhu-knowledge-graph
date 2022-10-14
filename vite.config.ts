b441605538853d1d47139a6f29208674ec3360bf
cccdev <lujt66@163.com>
2022-10-14 21:42:58 +0800

    chore: lint

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,13 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
     server: {
         proxy: {
-            "/api": {
-                target: "http://localhost:8085",
+            '/api': {
+                target: 'http://localhost:8085',
                 changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, ""),
+                rewrite: (path) => path.replace(/^\/api/, ''),
             },
         },
     },
