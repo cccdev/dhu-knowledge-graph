64dac5133f307803120338bdee0bafc8f3ccee97
cccdev <lujt66@163.com>
2022-10-18 01:10:30 +0800

    chore: lint code

--- src/components/MidContent/index.tsx
+++ src/components/MidContent/index.tsx
@@ -11,11 +11,13 @@ import React, { useEffect, useState } from 'react'
 import Point from '../Points'
 import './index.less'
 
-export class MidContentProps { }
+export class MidContentProps {}
 
 const MidContent: React.FC<MidContentProps> = (props) => {
     const [pointData, setPointData] = useState<GraphPoint[]>([])
-    const [routes, setRoutes] = useState<GraphPoint[]>([{ pointId: 'index', pointName: '首页' }])
+    const [routes, setRoutes] = useState<GraphPoint[]>([
+        { pointId: 'index', pointName: '首页' },
+    ])
     const initPoints = () => {
         request({
             url: '/home/toHome',
@@ -31,7 +33,7 @@ const MidContent: React.FC<MidContentProps> = (props) => {
         request({
             url: '/home/detail',
             method: 'post',
-            params: { pointId }
+            params: { pointId },
         }).then((res) => {
             if (res.code === 0) {
                 setPointData(res.data as GraphPoint[])
@@ -56,11 +58,11 @@ const MidContent: React.FC<MidContentProps> = (props) => {
         })
     }
     const jumpTo = (pointId: string) => {
-        let temp = "";
+        let temp = ''
         while (routes.length && temp != pointId)
-            temp = routes.pop()?.pointId as string;
+            temp = routes.pop()?.pointId as string
         if (temp === 'index')
-            routes.push({ pointId: 'index', pointName: '首页' });
+            routes.push({ pointId: 'index', pointName: '首页' })
         setRoutes(routes)
     }
     useEffect(() => {
@@ -69,19 +71,21 @@ const MidContent: React.FC<MidContentProps> = (props) => {
     return (
         <Content style={{ padding: '0 50px' }}>
             <Breadcrumb style={{ marginBottom: '15px' }}>
-                {
-                    routes.map(e => (
-                        <React.Fragment key={e.pointId}>
-                            <Breadcrumb.Item
-                                className='bread-crumb-item'
-                                onClick={
-                                    () => { jumpTo(e.pointId); e.pointId === 'index' ? initPoints() : getNextPointList(e.pointId, e.pointName) }
-                                }>
-                                {e.pointName}
-                            </Breadcrumb.Item>
-                        </React.Fragment>
-                    ))
-                }
+                {routes.map((e) => (
+                    <React.Fragment key={e.pointId}>
+                        <Breadcrumb.Item
+                            className="bread-crumb-item"
+                            onClick={() => {
+                                jumpTo(e.pointId)
+                                e.pointId === 'index'
+                                    ? initPoints()
+                                    : getNextPointList(e.pointId, e.pointName)
+                            }}
+                        >
+                            {e.pointName}
+                        </Breadcrumb.Item>
+                    </React.Fragment>
+                ))}
             </Breadcrumb>
             <div className="site-layout-content">
                 <Point data={pointData} getNextPointList={getNextPointList} />
