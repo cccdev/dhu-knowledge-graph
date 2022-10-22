2c7beacbf4cb6790a4008f9d638f200fa552bed5
Asea <569389750@qq.com>
2022-10-22 15:47:03 +0800

    feat: language

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.less'
+import zhCN from 'antd/es/locale/zh_CN';
+import { ConfigProvider } from 'antd'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <App></App>
+        <ConfigProvider locale={zhCN}>
+            <App></App>
+        </ConfigProvider>
     </React.StrictMode>
 )
