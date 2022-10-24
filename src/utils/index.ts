145569ad7586e13916b531220e1d7b75474047d2
Asea <569389750@qq.com>
2022-10-25 00:25:41 +0800

    feat: tree style)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -25,6 +25,7 @@ export const getTreeSeries = data => [
         bottom: '1%',
         right: '20%',
         symbolSize: 14,
+        initialTreeDepth: 2,
         label: {
             position: 'left',
             verticalAlign: 'middle',
@@ -52,7 +53,7 @@ export const getTreeMapSeries = data => [
         type: 'treemap',
         data: data[0]?.children,
         drillDownIcon: '▶',
-        leafDepth: 3,
+        leafDepth: 4,
         squareRatio: 1,
         label: {
             show: true,
